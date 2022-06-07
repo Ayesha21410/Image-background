497864aedcc35eff8055c7aede854012cc9c2af0
Ayesha Amjad <>
2022-06-07 01:09:33 +0000

    inserting title (heading label)

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ class QuizStarter:
     def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
         background_color1="#f0dff2"
         background_color="#a3e4fa"
+        self.title_image = Image.open("Title.png") #need to use Image if need to resize 
+        self.title_image = self.title_image.resize((295, 135), Image.ANTIALIAS)
+        self.title_image = ImageTk.PhotoImage(self.title_image)
+        
+        self.heading_label=Label(parent, image=self.title_image, border=0)
+        self.heading_label.place(x=170, y=110) 
