6cf08198e6f4ea8ea9101e4bbc5fc4aade7d9315
Ayesha Amjad <>
2022-06-07 01:11:13 +0000

    Insert entry box for name

--- main.py
+++ main.py
@@ -17,3 +17,6 @@ class QuizStarter:
              #label for username
         self.user_label=Label(parent, text="Please enter your username below: ", font=("Tw Cen MT","16"),bg=background_color)
         self.user_label.place(x=120, y=260) 
+             #entry box
+        self.entry_box=Entry(parent)
+        self.entry_box.place(x=230, y=320)
