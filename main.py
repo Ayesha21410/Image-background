c5c94ca1facc586201b264b6e7a33ed7a32e9cb0
Ayesha Amjad <>
2022-06-07 01:08:35 +0000

    Inserting first class and backgrounds

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from PIL import Image, ImageTk # for Images
 from tkinter import messagebox  # for error messages
 import random
 names_list = [] 
+#Component 1
+class QuizStarter:
+    def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
+        background_color1="#f0dff2"
+        background_color="#a3e4fa"
